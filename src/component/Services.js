f73e6613f7235c0a623b309f8e61b0646ee16a11
tsukeman <tsukeman@naver.com>
2024-03-15 12:10:11 +0900

    서비스 분야 디자인 수정

--- src/component/Services.js
+++ src/component/Services.js
@@ -1,57 +1,56 @@
-
 function Services() {
-    return (
-        <section className="page-section" id="services">
-        {/*<!-- Services-->*/}
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">Services</h2>
-            <h3 className="section-subheading text-muted">
-              Lorem ipsum dolor sit amet consectetur.
-            </h3>
-          </div>
-          <div className="row text-center">
-            <div className="col-md-4">
-              <span className="fa-stack fa-4x">
-                <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                <i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 className="my-3">E-Commerce</h4>
-              <p className="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-            <div className="col-md-4">
-              <span className="fa-stack fa-4x">
-                <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 className="my-3">Responsive Design</h4>
-              <p className="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-            <div className="col-md-4">
-              <span className="fa-stack fa-4x">
-                <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                <i className="fas fa-lock fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 className="my-3">Web Security</h4>
-              <p className="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-          </div>
+  return (
+    <section className="page-section" id="services">
+      {/*<!-- Services-->*/}
+      <div className="container-lg">
+        <div className="text-center">
+          <h2 className="section-heading text-uppercase">PAYsm 사업범위</h2>
         </div>
-      </section>
-    )
-}
+        <div className="row text-center pt-4">
+          <div className="col-md-4 text-start">
+            <div className="service-box">
+              <div>
+                <i class="fa-solid fa-headset"></i>
+              </div>
 
+              <h4 className="my-3">전자결제서비스</h4>
+              <p className="text-muted">
+                온라인결제, 정기결제, 간편결제 등 다양한 결제 서비스를
+                제공합니다.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-4 text-start">
+          <div className="service-box">
+              <div>
+                <i class="fa-solid fa-headset"></i>
+              </div>
+
+              <h4 className="my-3">회원관리ERP</h4>
+              <p className="text-muted">
+                회원관리 ERP를 통해 다양한 사업장의 회원관리 서비스를 제공합니다.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-4 text-start">
+          <div className="service-box">
+              <div>
+                <i class="fa-solid fa-headset"></i>
+              </div>
+
+              <h4 className="my-3">쇼핑몰 및 SI서비스</h4>
+              <p className="text-muted">
+                쇼핑몰의 구축 및 다양한 웹서비스 개발, 구축을 지원합니다. &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+}
 
 export default Services;
