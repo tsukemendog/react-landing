beff14c190238294fccafc3974ff3b0b579511df
tsukeman <tsukeman@naver.com>
2024-03-15 10:19:45 +0900

    Masthead 디자인 변경

--- src/component/Masthead.js
+++ src/component/Masthead.js
@@ -1,21 +1,19 @@
 function Masthead() {
     return (
-      <header
-      className="masthead"
-      style={{ backgroundImage: "url(/img/header-bg.jpg)" }}
-    >
-    {/*<!-- Masthead-->*/}
-      <div className="container">
-        <div className="masthead-subheading">Welcome To Our Studio!</div>
-        <div className="masthead-heading text-uppercase">
-          It's Nice To Meet You
-        </div>
-        <a className="btn btn-primary btn-xl text-uppercase" href="#services">
-          Tell Me More
-        </a>
-      </div>
-    </header>
+<header className="masthead">
+  {/* 배경 이미지를 위한 div 추가 */}
+  <div className="background-image" style={{ backgroundImage: "url(/img/banner/banner-building.webp)", backgroundSize: "cover" }}></div>
+  
+  {/* 기존 컨테이너 및 텍스트 */}
+  <div className="container">
+    <div className="masthead-heading text-uppercase">PAY + Prism </div>
+    <div className="masthead-heading text-uppercase">
+      금융의 다양한 혜택을 한번에
+    </div>
+  </div> 
+</header>
     )
 }
 
 export default Masthead;
+ 
